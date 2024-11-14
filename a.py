8e1d74c341db7ce753cbc86adc0be38efa855742
Parashu03 <gattiparashu03@gmail.com>
2024-11-14 14:28:43 +0530

    add new file a.py via upload

---
+++ a.py
@@ -0,0 +1,24 @@
+<<<<<<< Updated upstream
+
+while(5)
+    print("haha!")
+
+
+print("hello WORLD ")
+print("what is your name")
+print("bye ..")
+
+=======
+a
+b
+
+c
+d
+
+q
+w
+we
+e
+e
+e
+>>>>>>> Stashed changes
